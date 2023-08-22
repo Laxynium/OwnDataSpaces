fc19ba7b570518e4deb6eb23c98d58c15506cdc4
Laxynium <grzesiu15432@gmail.com>
2023-08-22 18:17:05 +0200

    Extract database to own file and outer scope

--- OwnDataSpaces.SqlServer.Tests/Replace_existing_unique_indexes_spec.cs
+++ OwnDataSpaces.SqlServer.Tests/Replace_existing_unique_indexes_spec.cs
@@ -1,12 +1,4 @@
-﻿using Dapper;
-using FluentAssertions;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using OwnDataSpaces.Configuration;
-using OwnDataSpaces.EFCore;
-using OwnDataSpaces.SqlServer.Tests.Fixtures;
-using Xunit.Abstractions;
+﻿using Xunit.Abstractions;
 
 namespace OwnDataSpaces.SqlServer.Tests;
 
@@ -301,7 +293,7 @@ public class ApplyOwnSpaceOnTablesWithIndexesSpec
             "SELECT COUNT(1) FROM [Table1]", 1);
     }
 
-    
+
     [Fact(Skip = "Looks like there cannot be a foreign key to filtered unique index, " +
                  "keeping this test as a documentation of this fact")]
     public async Task When_there_is_foreign_key_to_filtered_unique_index()
@@ -332,83 +324,5 @@ public class ApplyOwnSpaceOnTablesWithIndexesSpec
         await db.EnsureOwnSpacesAreNotLeaking(
             "INSERT INTO [Table1] (AltId, IsDeleted) VALUES(NEWID(), 0)",
             "SELECT COUNT(1) FROM [Table1]", 1);
-    }
-    
-
-    private class Database
-    {
-        private IServiceProvider _serviceProvider;
-        public string ConnectionString { get; }
-        public string SchemaName { get; }
-
-        private Database(string connectionString, string schemaName, IServiceProvider serviceProvider)
-        {
-            ConnectionString = connectionString;
-            SchemaName = schemaName;
-            _serviceProvider = serviceProvider;
-        }
-
-        public static async Task<Database> CreateDatabase(string dbName)
-        {
-            var container = new SqlServerContainer(dbName);
-            await container.InitializeAsync();
-            await using (var masterConnection =
-                         new SqlConnection(SqlServerContainer.ReplaceDatabase(container.ConnectionString, "master")))
-            {
-                await masterConnection.ExecuteAsync($"""
-                                                     DROP DATABASE IF EXISTS [{dbName}];
-                                                     CREATE DATABASE [{dbName}];                                                          
-                                                     """);
-            }
-
-            var services = new ServiceCollection();
-            services.AddLogging(l => l.ClearProviders());
-            services.AddOwnSpaces(opt => opt.UseSqlServer());
-
-            return new Database(container.ConnectionString, "dbo", services.BuildServiceProvider());
-        }
-
-        public async Task Execute(string sql)
-        {
-            await using var connection = new SqlConnection(ConnectionString);
-            await connection.ExecuteAsync(sql);
-        }
-
-        public async Task EnsureOwnSpacesAreNotLeaking(string writeSql, string readSql, int expectedCount)
-        {
-            var ownSpace1 = new OwnSpace(() => new HttpClient(), () => _serviceProvider);
-            await Write(ownSpace1, writeSql);
-
-            var ownSpace2 = new OwnSpace(() => new HttpClient(), () => _serviceProvider);
-            await Write(ownSpace2, writeSql);
-
-            var ownSpace1ActualCount = await Read(ownSpace1, readSql);
-            ownSpace1ActualCount.Should().Be(expectedCount);
-
-            var ownSpace2ActualCount = await Read(ownSpace2, readSql);
-            ownSpace2ActualCount.Should().Be(expectedCount);
-        }
-
-        private async Task Write(OwnSpace ownSpace, string sql)
-        {
-            await using var scope = ownSpace.GetAsyncScope();
-            await using var connection = new SqlConnection(ConnectionString);
-            await connection.OpenAsync();
-            var setOwnSpace = scope.ServiceProvider.GetRequiredService<SetOwnSpaceSqlConnection>();
-            await setOwnSpace.SetOwnSpace(connection, default);
-
-            await connection.ExecuteAsync(sql);
-        }
-
-        private async Task<int> Read(OwnSpace ownSpace, string sql)
-        {
-            await using var scope = ownSpace.GetAsyncScope();
-            await using var connection = new SqlConnection(ConnectionString);
-            await connection.OpenAsync();
-            var setOwnSpace = scope.ServiceProvider.GetRequiredService<SetOwnSpaceSqlConnection>();
-            await setOwnSpace.SetOwnSpace(connection, default);
-
-            return await connection.QuerySingleAsync<int>(sql);
-        }
     }
 }
